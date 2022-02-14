0bb273bfdb29bc0f2e135448af3495eb5d183b51
Aliza Aziz <83037489+Aliza02@users.noreply.github.com>
2022-02-14 20:57:47 +0500

    Add files via upload

---
+++ loginpage/loggin.js
@@ -0,0 +1,24 @@
+
+			function signup() {
+				 document.getElementById('loginarea').style.display="none"; 
+				 document.getElementById('signin').style.display="none"; 
+				 document.getElementById('signup').classList.remove("d-none");
+				 document.getElementById('signup').style.display="block"; 
+
+			} 
+			function signin() {
+				 document.getElementById('loginarea').style.display="none"; 
+				 document.getElementById('signup').style.display="none"; 
+				 document.getElementById('signin').classList.remove("d-none");
+				 document.getElementById('signin').style.display="block"; 
+			}
+			// signin
+
+			// function signinfields()
+			// {
+			// 	var email_user=document.getElementById('emailinput').value;
+			// 	if( email_user!="")
+			// 	{
+					
+			// 	}
+			// }
